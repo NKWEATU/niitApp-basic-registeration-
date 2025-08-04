88a14d35f1320062acdd16cf18f00ecb0470a67f
Kennedy <nkweatukennedy@gmail.com>
2025-08-04 11:05:01 +0100

    NIIT_Registration_Form

---
+++ src/main/java/com/mycompany/myniitapp/MyNiitApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myniitapp;
+
+/**
+ *
+ * @author USER
+ */
+public class MyNiitApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/db.java
@@ -0,0 +1,32 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class db {
+    
+       public static Connection mycon(){
+       Connection con = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            //use this line of code for mysql in xamp "jdbc:mysql://127.0.0.1:3306/user_registeration_database","root",""
+                                
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/niit_app","root","Kenny4640");
+            
+        }catch (ClassNotFoundException | SQLException e ) {
+        System.out.println(e);
+        }
+        
+        return con;
+    }
+}
